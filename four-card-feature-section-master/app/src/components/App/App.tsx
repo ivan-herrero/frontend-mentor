e99dfc8d14abfc3fadd853636c44df0819303201
Iván Herrero <ivan.herreronunez@vistaprint.com>
2020-04-12 20:01:56 +0200

    Set up index

--- four-card-feature-section-master/app/src/components/App/App.tsx
+++ four-card-feature-section-master/app/src/components/App/App.tsx
@@ -4,7 +4,23 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      App
+      Reliable, efficient delivery
+      Powered by Technology
+
+      Our Artificial Intelligence powered tools use millions of project data points
+      to ensure that your project is successful
+
+      Supervisor
+      Monitors activity to identify project roadblocks
+
+      Team Builder
+      Scans our talent network to create the optimal team for your project
+
+      Karma
+      Regularly evaluates our talent to ensure quality
+
+      Calculator
+      Uses data from past projects to provide better delivery estimates
     </div>
   );
 }
